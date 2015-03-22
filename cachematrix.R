e4fc763a9292dd151dd946a5ecea75c446fc73af
skhurana11 <skhurana11@gmail.com>
2015-03-21 23:18:46 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -33,18 +33,56 @@
 ##      m
 ## }
 ## BEGIN ASSIGNMENT
-## Put comments here that give an overall description of what your
-## functions do
+## PART 1 OF THE ASSIGNMENT
+## To write a pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special  "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## this will save the cached value or NULL if nothing is cached
+## initially nothing is cached so the default is set to NULL
+cache <- NULL
+
+# setMatrix stores a matrix
+setMatrix <- function(newValueMatrix) {
+        x <<- newValueMatrix
+        # the matrix is given a new value, flush the cache
+        cache <<- NULL
 }
+getMatrix <- function() {
+        x
+}
+## this will return the stored matrix
+getInverse <- function() {
+        cache
+}
+## cache the given argument
+list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+}
+## this will return a list
+## each element of the list is a function
 
-
-## Write a short comment describing this function
+## PART 2 OF THE ASSIGNMENT
+## this function computes the inverse of the special "matrix" returned by makeCacheMatrix below.  
+## if the inverse has already been calculated (and the the matrix has not changed), then the cachesolve should retrieve the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- y$getInverse()
+        ## get the cached value
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+}
+## if there is a cached value, this function will return it
+
+## if not, get the matrix, calculate the inverse and store in the cache
+data <- y$getMatrix()
+inverse <- solve(data)
+y$cacheInverse(inverse)
+
+## this will return the inverse
+inverse
 }
